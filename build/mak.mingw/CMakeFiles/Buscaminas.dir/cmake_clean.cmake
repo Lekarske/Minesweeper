8c9aa1221ba2e7dfc512969e37abda21a369f90a
Adrian R <Lekarske@hotmail.com>
2017-09-30 18:40:00 -0500

    Team work

---
+++ build/mak.mingw/CMakeFiles/Buscaminas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Buscaminas.dir/buscaminas.cpp.obj"
+  "Buscaminas.pdb"
+  "Buscaminas.exe"
+  "Buscaminas.exe.manifest"
+  "libBuscaminas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Buscaminas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/mak.mingw/CMakeFiles/Buscaminas.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Buscaminas.
+# This may be replaced when dependencies are built.
